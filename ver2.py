ae0c6b079f8c4ff8b147fd34172e3d4da5876b8c
Mosogomak <102731508+GodSaeng@users.noreply.github.com>
2022-05-04 00:50:26 +0900

    update ver2.py

---
+++ ver2.py
@@ -0,0 +1,15 @@
+#이상형 질문게시판 ver2. [{'질문':'취미는?','답변':'먹기'},{'질문':'특기','답변':'피아노'}]
+total_list = []
+
+dict={}
+while True:
+    question = input("질문을 입력해주세요 : ")
+    if question == 'q':
+        break
+    else:
+        dict['질문'] = question
+        total_list.append(dict)
+
+
+print(dict)
+print(total_list)
