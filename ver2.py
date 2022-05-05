1901594dc042d4c7be5658fa42835f76cb883abc
Mosogomak <102731508+GodSaeng@users.noreply.github.com>
2022-05-05 22:29:35 +0900

    최종

--- ver2.py
+++ ver2.py
@@ -1,15 +1,17 @@
 #이상형 질문게시판 ver2. [{'질문':'취미는?','답변':'먹기'},{'질문':'특기','답변':'피아노'}]
 total_list = []
 
-dict={}
 while True:
     question = input("질문을 입력해주세요 : ")
     if question == 'q':
         break
     else:
-        dict['질문'] = question
-        print(dict)
-        total_list.append(dict)
-        print(total_list)
+        total_list.append({"질문" : question, "답변" : ""})
 
 
+for k in total_list:
+    print(k['질문'])
+    answer = input("답변을 입력해주세요 : ")
+    k['답변'] = answer
+
+print(total_list)
